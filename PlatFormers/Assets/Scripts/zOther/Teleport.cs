118ad8caf5d7083912ed681be9afa5ba502ee19f
LMD9977 <114038919+LMD9977@users.noreply.github.com>
2024-02-19 08:20:10 -0500

    added telipoertes
    Signed-off-by: LMD9977 <114038919+LMD9977@users.noreply.github.com>

---
+++ PlatFormers/Assets/Scripts/zOther/Teleport.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] public GameObject player;
+    [SerializeField] public GameObject trapTarget;
+    public Teleport tel;
+    public int numberTelort = 0;
+    public MagicMissisal  missisal;
+    public HealthRegen healthRegen;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        missisal.enabled = false;
+        healthRegen.enabled = false;
+
+    }
+    private void Update()
+    {
+        if (tel.numberTelort >= 2) missisal.enabled = true;
+        if(tel.numberTelort >= 3) healthRegen.enabled = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collistion = collision.gameObject;
+        if (collistion.CompareTag("Player"))
+        {
+            tel.numberTelort += 1;
+            player.transform.position = trapTarget.transform.position;
+        }
+    }
+}
